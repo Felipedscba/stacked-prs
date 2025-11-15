089517dcc023e6d0cf7a1280446d254f89405a96
Felipedscba <felipedscba@gmail.com>
2025-11-15 16:59:08 -0300

    feat: Add healthy check route and start script on package.json

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
-function main() {
-  // Your code goes here
-}
+const server = Bun.serve({
+    port: 3000,
+    routes: {
+        '/': () => Response.json({ message: 'Application is running' }, { status: 200 }),
+    },
+});
+
+console.log(`Server running at ${server.url}`);
